45812e1d22b1f484ff4255dbcb1e04ae79fdad19
AShunevich <48925983+AShunevich@users.noreply.github.com>
2022-10-09 11:14:07 +0300

    - code update

--- app/src/main/java/ashunevich/uniconverterKT/currencyapi/backend/InternetService.kt
+++ app/src/main/java/ashunevich/uniconverterKT/currencyapi/backend/InternetService.kt
@@ -39,17 +39,15 @@ class InternetService {
 
 
     // CHECK WHETHER INTERNET CONNECTION IS AVAILABLE OR NOT
-    protected fun checkConnection(context: Context): Boolean {
+     fun checkConnection(context: Context): Boolean {
         val connMgr = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (connMgr != null) {
-            val activeNetworkInfo = connMgr.activeNetworkInfo
-            if (activeNetworkInfo != null) { // connected to the internet
-                // connected to the mobile provider's data plan
-                return if (activeNetworkInfo.type == ConnectivityManager.TYPE_WIFI) {
-                    // connected to wifi
-                    true
-                } else activeNetworkInfo.type == ConnectivityManager.TYPE_MOBILE
-            }
+        val activeNetworkInfo = connMgr.activeNetworkInfo
+        if (activeNetworkInfo != null) { // connected to the internet
+            // connected to the mobile provider's data plan
+            return if (activeNetworkInfo.type == ConnectivityManager.TYPE_WIFI) {
+                // connected to wifi
+                true
+            } else activeNetworkInfo.type == ConnectivityManager.TYPE_MOBILE
         }
         return false
     }
