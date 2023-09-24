6894376cf30d990f65a9476dcfbb775a39b0a367
AShunevych <shunevichanton@gmail.com>
2023-09-24 18:16:52 +0300

    - code update

--- app/src/androidTest/java/ashunevich/uniconverterKT/common/BasicRule.kt
+++ app/src/androidTest/java/ashunevich/uniconverterKT/common/BasicRule.kt
@@ -1,10 +1,19 @@
 package ashunevich.uniconverterKT.common
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import ashunevich.uniconverterKT.ActivityMain
+import com.emergetools.snapshots.EmergeSnapshots
 import org.junit.Rule
+import org.junit.rules.RuleChain
+import org.junit.runner.RunWith
 
 open class BasicRule {
-    @get:Rule
+    val snapshots = EmergeSnapshots()
     val activityRule = ActivityScenarioRule(ActivityMain::class.java)
+
+    @get:Rule
+    val ruleChain: RuleChain = RuleChain
+        .outerRule(snapshots)
+        .around(activityRule);
 }
---
+++ app/src/androidTest/java/ashunevich/uniconverterKT/espresso/tests/screenshot/ScreenshotTests.kt
@@ -0,0 +1,18 @@
+package ashunevich.uniconverterKT.espresso.tests.screenshot
+
+import ashunevich.uniconverterKT.common.BasicRule
+import ashunevich.uniconverterKT.espresso.robots.converterRobot
+import org.junit.Test
+
+class ScreenshotTests : BasicRule() {
+
+    @Test
+    fun mainActivitySnapshot() {
+        converterRobot {
+            verify()
+        }
+        activityRule.scenario.onActivity {
+            snapshots.take(name = "MainActivity", activity = it)
+        }
+    }
+}
